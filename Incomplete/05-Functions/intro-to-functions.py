b6eb4a07c429ebfcbc0a39e1774162a8d0a233fb
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2023-10-23 18:02:06 -0400

    eod commit

---
+++ Incomplete/05-Functions/intro-to-functions.py
@@ -0,0 +1,7 @@
+def output_text():
+    # pass # pass continues allows to be a function that does nothing.
+    print("Welcome to the program!")
+    print("It's nice to meet you")
+    print("Have a wonderful dat programming!")
+
+output_text()
